dd09974ac250be178510c75a96e70addf3d1752b
shoaib30 <shoaib30@gmail.com>
2018-04-06 23:04:47 +0530

    updated routes

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var users = require('./routes/users');
 
 var app = express();
 
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -18,7 +20,9 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -26,14 +30,14 @@ app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -42,5 +46,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,105 @@
 var express = require('express');
 var router = express.Router();
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('database', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'sqlite',
+  operatorsAliases: false,
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+
+  // SQLite only
+  storage: 'data.db'
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully with database.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+const Beneficiary = sequelize.define('beneficiary', {
+  name: {
+    type: Sequelize.STRING
+  },
+  tokenId: {
+    type: Sequelize.STRING,
+    primaryKey: true
+  },
+  status: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false
+  }
+});
+
+Beneficiary.sync();
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express'
+  });
+});
+
+/*
+[
+	{
+		"name":"Shoaib Ahmed",
+		"tokenId":"QWERTY123"
+	}
+]
+*/
+router.post('/load-data', function (req, res, next) {
+  var benData = req.body;
+
+  for (i in benData) {
+    var beneficiary = benData[i]
+    Beneficiary.create(beneficiary)
+  }
+  res.send('data stored');
+});
+
+router.get('/all-data', function (req, res, next) {
+  Beneficiary.findAll().then(beneficiaries => {
+    res.end(JSON.stringify(beneficiaries));
+  });
+});
+
+router.put('/update-status', function (req, res, next) {
+  var tokenId = req.query.tokenId;
+  Beneficiary.findById(tokenId).then(beneficiary => {
+    if (beneficiary == null) {
+      res.status(404);
+      res.end('Token Not Found');
+    }
+    Beneficiary.update({
+        status: true
+      }, {
+        where: {
+          'tokenId': tokenId
+        }
+      })
+      .then(() => {
+        res.end('Token Updated');
+      });
+  });
+});
+
+router.delete('/flush-data', function (req, res, next) {
+  Beneficiary.destroy({
+    truncate: true
+  }).then(() => {
+    res.end('Cleared all data')
+  });
 });
 
 module.exports = router;
